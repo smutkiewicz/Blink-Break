8ebd294f491039b0c16de345b04a6518a1e81e65
smutkiewicz <smutkiewicz@gmail.com>
2018-07-14 13:15:53 +0200

    StatsHelper issue solved, notification policy changed

--- app/src/main/java/com/smutkiewicz/blinkbreak/util/StatsHelper.kt
+++ app/src/main/java/com/smutkiewicz/blinkbreak/util/StatsHelper.kt
@@ -2,7 +2,7 @@ package com.smutkiewicz.blinkbreak.util
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import java.lang.Math.abs
+import org.joda.time.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,6 +10,12 @@ class StatsHelper(internal var context: Context) {
     internal var pref: SharedPreferences
     internal var editor: SharedPreferences.Editor
     internal var spName: String
+
+    init {
+        spName = context.packageName + "_preferences"
+        pref = context.getSharedPreferences(spName, MODE_PRIVATE)
+        editor = pref.edit()
+    }
 
     var unskippedBreaks: Int
         get() = pref.getInt(STAT_UNSKIPPED_BREAKS, 0)
@@ -32,12 +38,6 @@ class StatsHelper(internal var context: Context) {
             editor.commit()
         }
 
-    init {
-        spName = context.packageName + "_preferences"
-        pref = context.getSharedPreferences(spName, MODE_PRIVATE)
-        editor = pref.edit()
-    }
-
     fun increaseValue(prefName: String) {
         when(prefName) {
             STAT_UNSKIPPED_BREAKS -> {
@@ -55,23 +55,27 @@ class StatsHelper(internal var context: Context) {
 
     fun calculateTimeDifference(): String {
         if (lastBreak != STAT_NEVER) {
-            val currentTime = getTimeStamp()
-            val format = SimpleDateFormat("HH:mm:ss")
-            val date1 = format.parse(lastBreak)
-            val date2 = format.parse(currentTime)
-            val difference = abs(date2.time - date1.time)
+            val format = SimpleDateFormat(DATE_FORMAT)
+            val currentDate = getTimeStamp()
 
-            var seconds = (difference / 1000)
-            val minutes = (seconds / 60)
-            val hours = (minutes / 60)
+            val d1 = format.parse(lastBreak)
+            val d2 = format.parse(currentDate)
 
-            seconds %= 60
+            val dt1 = DateTime(d1)
+            val dt2 = DateTime(d2)
+
+            val seconds = Seconds.secondsBetween(dt1, dt2).seconds % 60
+            val minutes = Minutes.minutesBetween(dt1, dt2).minutes % 60
+            val hours = Hours.hoursBetween(dt1, dt2).hours % 24
+            val days = Days.daysBetween(dt1, dt2).days
 
             return when {
-                hours > 0 -> String.format("%d", seconds) + " hour(s) ago."
-                minutes > 0 -> String.format("%d", minutes) + " min(s) ago."
-                else -> String.format("%d", seconds) + " second(s) ago."
+                days > 0 -> days.toString() + " day(s) ago."
+                hours > 0 -> hours.toString() + " hour(s) ago."
+                minutes > 0 -> minutes.toString() + " min(s) ago."
+                else -> seconds.toString() + " second(s) ago."
             }
+
         } else {
             return STAT_NEVER
         }
@@ -84,9 +88,10 @@ class StatsHelper(internal var context: Context) {
         val STAT_MAXIMAL_BREAK_DURATION = "stat_max_break_duration"
         val STAT_UNSKIPPED_BREAKS_DURATION = "stat_unskipped_break_duration"
         val STAT_NEVER = "Never"
+        val DATE_FORMAT = "MM/dd/yyyy HH:mm:ss"
 
         fun getTimeStamp() =
-                SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().time)
+                SimpleDateFormat(DATE_FORMAT).format(Calendar.getInstance().time)!!
 
     }
 
